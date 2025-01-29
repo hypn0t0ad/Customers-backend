267414b371b9133f5a656a47f81a74e5d844a63d
christian dahlgren <christian.dahlgren84@gmail.com>
2025-01-29 11:04:40 +0100

    Commit-point

--- src/main/java/com/example/customers_backend/config/SecurityConfig.java
+++ src/main/java/com/example/customers_backend/config/SecurityConfig.java
@@ -8,17 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.client.OAuth2AuthorizationContext;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import java.net.URI;
 
 @Configuration
 @EnableWebSecurity
@@ -39,34 +30,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http, ClientRegistrationRepository clientRegistrationRepository) throws Exception {
-//        http
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/", "/login", "/error").permitAll()
-//                        .anyRequest().authenticated()
-//                )
-//                .oauth2Login(oauth2 -> oauth2.loginPage("/login"))
-//                .logout(logout -> logout
-//                        .logoutSuccessHandler(oidcLogoutHandler(clientRegistrationRepository))
-//                        .invalidateHttpSession(true)
-//                        .deleteCookies("JSESSIONID")
-//                        .clearAuthentication(true)
-//                )
-//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
-//
-//        return http.build();
-//    }
-
-//    @Bean
-//    public OidcClientInitiatedLogoutSuccessHandler oidcLogoutHandler(ClientRegistrationRepository clientRegistrationRepository) {
-//        OidcClientInitiatedLogoutSuccessHandler logoutSuccessHandler =
-//                new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
-//
-//        // Redirect to Keycloak logout endpoint after logging out
-//        logoutSuccessHandler.setPostLogoutRedirectUri(String.valueOf(URI.create("http://localhost:5173")));
-//        return logoutSuccessHandler;
-//    }
 }
--- src/main/java/com/example/customers_backend/controller/CustomerController.java
+++ src/main/java/com/example/customers_backend/controller/CustomerController.java
@@ -4,7 +4,6 @@ import com.example.customers_backend.entity.Customer;
 import com.example.customers_backend.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
@@ -31,5 +30,23 @@ public class CustomerController {
     public ResponseEntity<Void> deleteCustomer(@PathVariable Long id) {
         customerRepository.deleteById(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("{/id}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable long id) {
+        return ResponseEntity.ok(customerRepository.findById(id)
+                .orElseThrow(RuntimeException::new));
+    }
+
+    @PostMapping("{/id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable long id,
+                                                @RequestBody Customer customer) {
+        Customer customerToModify = customerRepository.getReferenceById(id);
+        customerToModify.setName(customer.getName());
+        customerToModify.setEmail(customer.getEmail());
+        customerToModify.setAddress(customer.getAddress());
+        customerToModify.setPhonenumber(customer.getPhonenumber());
+        customerRepository.save(customerToModify);
+        return ResponseEntity.ok(customerToModify);
     }
 }
